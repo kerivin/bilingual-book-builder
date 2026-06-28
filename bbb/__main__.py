c5773a68d82927b4a6862cd5af6fe189452f11f5
kerivin <kerivin.zero@gmail.com>
2026-06-29 00:05:02 +0300

    minimal running program

--- bbb/__main__.py
+++ bbb/__main__.py
@@ -4,10 +4,10 @@ from bertalign.bertalign import Bertalign
 def main() -> int:
 	print("Hello world, bilingual book builder is alive!")
 
-	src_sents = ["Hello.", "How are you?"]
-	tgt_sents = ["Bonjour.", "Comment allez-vous ?"]
+	src_text = "Hello. How are you?"
+	tgt_text = "Bonjour. Comment allez-vous ?"
 
-	aligner = Bertalign(src_sents, tgt_sents)
+	aligner = Bertalign(src=src_text, src_lang="en", tgt=tgt_text, tgt_lang="fr")
 	aligner.align_sents()
 	aligner.print_sents()
 	return 0
