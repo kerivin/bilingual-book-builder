541cdfc55d248d369ec9061fee7aa9bc37a51507
kerivin <kerivin.zero@gmail.com>
2026-07-11 03:21:26 +0300

    simple-split is now off by default

--- bbb/cli.py
+++ bbb/cli.py
@@ -19,7 +19,7 @@ def main() -> int:
     parser.add_argument('--keep-target-chapters', action='store_true', default=False, help='Whether to keep target chapters that have no source original')
     parser.add_argument('--align-model', type=str, default='LaBSE', help='Name or path to sentence aligner model (download LaBSE if omitted)')
     parser.add_argument('--split-model', type=str, default='sat-3l', help='Name or path to sentence splitter model (download sat-3l if omitted)')
-    parser.add_argument('--simple-split', action='store_true', default=True, help='Use heuristic sentence splitting instead of the split-model')
+    parser.add_argument('--simple-split', action='store_true', default=False, help='Use heuristic sentence splitting instead of the split-model')
     parser.add_argument('-v', '--verbosity', choices=['silent', 'progress', 'verbose'], default='progress', help='Silent (no progress), Progress (show progress bars), Verbose (all messages)')
     
     args = parser.parse_args()
