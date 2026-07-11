8f9a48028fc15e4b8e86796e86dc26c7947dc362
kerivin <kerivin.zero@gmail.com>
2026-07-11 04:37:29 +0300

    thread-safe ChapterSplitter

--- bbb/chapter_splitter.py
+++ bbb/chapter_splitter.py
@@ -1,4 +1,5 @@
 import logging
+import threading
 from lingua import Language
 from sentence_splitter import SentenceSplitter
 from wtpsplit import SaT
@@ -8,6 +9,7 @@ class ChapterSplitter:
         self.simple_split = model_name is None
         self.model_name = model_name
         self.models = {}
+        self.lock = threading.Lock()
         self.log = logging.getLogger(__name__)
     
     def run(self, text, language: Language):
@@ -15,18 +17,25 @@ class ChapterSplitter:
         return splitter.split(text)
         
     def _get_model(self, language: Language):
-        if not language in self.models:
+        if language in self.models:
+            return self.models[language]
+
+        with self.lock:
+            if language in self.models:
+                return self.models[language]
+
             if self.simple_split:
-                self.models[language] = SentenceSplitter(
+                splitter = SentenceSplitter(
                     language.iso_code_639_1.name.lower()
                 )
                 self.log.info(f"Created simple SentenceSplitter for {language.name}")
             else:
-                self.models[language] = SaT(
+                splitter = SaT(
                     model_name_or_model=self.model_name,
                     language=language.iso_code_639_1.name.lower() if language else None,
                     style_or_domain='ud' if language else None,
                 )
                 self.log.info(f"Created SaT for {language.name}")
-        
-        return self.models[language]
+
+            self.models[language] = splitter
+            return splitter
